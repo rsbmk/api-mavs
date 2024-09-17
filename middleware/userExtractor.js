6809161f2223b958ee20dd8d78a707e44292ecb0
Roberto Bocio Melo <robertobocio@01lab.co>
2024-09-16 23:19:20 -0500

    fix: jwt expiration

--- middleware/userExtractor.js
+++ middleware/userExtractor.js
@@ -1,23 +1,29 @@
-import jwt from 'jsonwebtoken'
+import jwt from "jsonwebtoken";
 
 export const jwtMiddleware = (request, response, next) => {
   // recuperamos el token de la cabecera para autorizar
-  const authorization = request.get('authorization')
+  const authorization = request.get("authorization");
 
-  let token = ''
+  let token = "";
 
-  if (authorization && authorization.toLocaleLowerCase().startsWith('bearer')) {
-    token = authorization.substring(7)
+  if (authorization && authorization.toLocaleLowerCase().startsWith("bearer")) {
+    token = authorization.substring(7);
   }
 
-  const decodeToken = jwt.verify(token, process.env.SECRET_JWT)
+  let decodeToken;
+  try {
+    decodeToken = jwt.verify(token, process.env.SECRET_JWT);
+  } catch (error) {
+    console.log({ error });
+    return response.status(401).json({ message: "no authorization" });
+  }
 
   if (!token || !decodeToken.id) {
-    return response.status(401).json({ message: 'no authorization' })
+    return response.status(401).json({ message: "no authorization" });
   }
 
-  const { id } = decodeToken
-  request.userId = id
+  const { id } = decodeToken;
+  request.userId = id;
 
-  next()
-}
+  next();
+};
