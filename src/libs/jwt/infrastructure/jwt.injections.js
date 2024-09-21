02f45a9c6a71c21b680062a368da0e0516e82746
Roberto Bocio Melo <elpicapica01@gmail.com>
2024-09-21 02:27:16 -0500

    fix: jwt service import

--- src/libs/jwt/infrastructure/jwt.injections.js
+++ src/libs/jwt/infrastructure/jwt.injections.js
@@ -1,6 +1,6 @@
 // @ts-check
 import jwt from "jsonwebtoken";
 
-import { JwtService } from "../application/jwt.service";
+import { JwtService } from "../application/jwt.service.js";
 
 export const jwtService = new JwtService(jwt);
