1864b25e1d7fbf1e40a656cc1b2497e284e43e17
Roberto Bocio Melo <elpicapica01@gmail.com>
2024-09-29 17:48:15 -0500

    refactor: don't use a user ir params, use an log session data

--- src/like/infrastructura/like.route.js
+++ src/like/infrastructura/like.route.js
@@ -1,8 +1,8 @@
 // @ts-check
 
 import { Router } from "express";
-import { likeController } from "./like.injections.js";
 import { jwtApiGuard } from "../../auth/infrastructure/jwtApi.guard.injections.js";
+import { likeController } from "./like.injections.js";
 
 export const likeRouter = Router();
 
@@ -10,5 +10,5 @@ likeRouter
   .use(jwtApiGuard.run.bind(jwtApiGuard))
   .post("/", likeController.create.bind(likeController))
   .get("/:characterId/character", likeController.findAllByCharacterId.bind(likeController))
-  .get("/:userId/user", likeController.findAllByUserId.bind(likeController))
+  .get("/user", likeController.findAllByUserId.bind(likeController))
   .delete("/:id", likeController.delete.bind(likeController));
