7714c4ce2e46118b5f82c1a8cf94654f87f4738f
Roberto Bocio Melo <elpicapica01@gmail.com>
2024-10-07 21:45:56 -0500

    fix: url for correct rest api rules

--- src/comments/infrastructure/comment.route.js
+++ src/comments/infrastructure/comment.route.js
@@ -1,15 +1,15 @@
 // @ts-check
 
 import { Router } from "express";
-import { commentController } from "./comment.injections.js";
 import { jwtApiGuard } from "../../auth/infrastructure/jwtApi.guard.injections.js";
+import { commentController } from "./comment.injections.js";
 
 export const commentRouter = Router();
 
 commentRouter
   .use(jwtApiGuard.run.bind(jwtApiGuard))
   .post("/", commentController.create.bind(commentController))
-  .get("/:characterId/character", commentController.findAllByCharacterId.bind(commentController))
-  .get("/:userId/user", commentController.findAllByUserId.bind(commentController))
+  .get("/characters/:characterId", commentController.findAllByCharacterId.bind(commentController))
+  .get("/user", commentController.findAllByUserId.bind(commentController))
   .put("/:id", commentController.update.bind(commentController))
   .delete("/:id", commentController.delete.bind(commentController));
