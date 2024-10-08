10a83038833815eed493834c9105a38c617cff2c
Roberto Bocio Melo <elpicapica01@gmail.com>
2024-10-07 22:00:15 -0500

    feat: create a customs partials types

--- src/common/utils.type.ts
+++ src/common/utils.type.ts
@@ -4,3 +4,9 @@ export type IUtils = {
   buildSuccessResponse: (message: string, data: any) => object;
   buildErrorResponse: (Error: Error, response: Response) => object;
 };
+
+export type RecursivePartial<T> = {
+  [P in keyof T]?: RecursivePartial<T[P]>
+}
+
+export type PartialExcept<T, K extends keyof T> = RecursivePartial<T> & Pick<T, K>
