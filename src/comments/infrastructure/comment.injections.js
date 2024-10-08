1d73edd63513109035e513b548dc387fb5fbe649
Roberto Bocio Melo <elpicapica01@gmail.com>
2024-10-07 21:46:13 -0500

    feat: add user services dependency

--- src/comments/infrastructure/comment.injections.js
+++ src/comments/infrastructure/comment.injections.js
@@ -1,10 +1,11 @@
 // @ts-check
 
-import { CommentController } from "./comment.controller.js";
+import { userService } from "../../user/infrastructure/user.injections.js";
 import { CommentService } from "../application/comment.service.js";
+import { CommentController } from "./comment.controller.js";
 import { CommentRepository } from "./comment.repository.js";
 import { CommentModel } from "./comments.model.js";
 
 const commentRepository = new CommentRepository(CommentModel);
-export const commentService = new CommentService(commentRepository);
+export const commentService = new CommentService(commentRepository, userService);
 export const commentController = new CommentController(commentService);
